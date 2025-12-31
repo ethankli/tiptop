9c4b5f946607072b799b35adcb3e641c76655cea
Ethan Lindley <ethankli@hotmail.com>
2025-12-31 01:55:46 -0600

    datagram: cleanliness

--- util/datagram.py
+++ util/datagram.py
@@ -2,12 +2,8 @@ import struct
 
 
 class Datagram:
-    def __init__(self, data: bytes = bytes()):
-        if len(data) == 0:
-            # Reserve first 2 bytes for length field
-            self.data = bytearray(b'\x00\x00')
-        else:
-            self.data = bytearray(data)
+    def __init__(self, data: bytes = bytes(b'\x00\x00')):
+        self.data = bytearray(data)
         self.length = 0
 
     def update_length(self):
